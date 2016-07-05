e11f9e90d896296bebe58e6c46fedd593b29340a
Kris Moore <kris@pcbsd.org>
2016-07-05 12:39:35 -0400

    Update nvidia driver

---
+++ x11/nvidia-driver/files/extra-patch-src_nvidia_nv-misc.h
@@ -0,0 +1,11 @@
+--- src/nvidia/nv-misc.h.orig	2015-10-30 22:24:29 UTC
++++ src/nvidia/nv-misc.h
+@@ -11,6 +11,8 @@
+ #ifndef _NV_MISC_H_
+ #define _NV_MISC_H_
+ 
++#include "opt_global.h"
++
+ #include "nvtypes.h"
+ #include "rmretval.h"
+ 
