75dfd1d74c7fc8e0dc41278e36b1af241e45239d
Ken Moore <ken@pcbsd.org>
2016-07-26 08:43:52 -0400

    Disable the USES += shared-mime-info backend in the TrueOS ports tree.   This basically elevates GTK2 as a requirement for all graphical pkgs   and this behaviour (updating *.desktop databases) is *not* needed for   many non-GNOME DE's.
      Re-evaluate this functionality if we can find an alternative C/C++  tool

--- Mk/Uses/shared-mime-info.mk
+++ Mk/Uses/shared-mime-info.mk
@@ -15,8 +15,15 @@ _INCLUDE_USES_SHARED_MIME_INFO_MK=	yes
 IGNORE=	USES=shared-mime-info does not require args
 .endif
 
-BUILD_DEPENDS+=	update-mime-database:misc/shared-mime-info
-RUN_DEPENDS+=	update-mime-database:misc/shared-mime-info
-PLIST_FILES+=	"@shared-mime-info share/mime"
+# Disable this on the TrueOS repo (7/26/16 - Ken Moore)
+#  This basically elevates GTK2 as a requirement for all graphical pkgs
+#  and this behaviour (updating *.desktop databases) is *not* needed for 
+#  many non-GNOME DE's.
+#
+#  Re-evaluate this functionality if we can find an alternative C/C++  tool
+
+#BUILD_DEPENDS+=	update-mime-database:misc/shared-mime-info
+#RUN_DEPENDS+=	update-mime-database:misc/shared-mime-info
+#PLIST_FILES+=	"@shared-mime-info share/mime"
 
 .endif
