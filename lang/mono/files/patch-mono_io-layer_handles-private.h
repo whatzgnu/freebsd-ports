38af5353c1a84d82f6ed0b01c8c0373eb3aa1544
Kris Moore <kris@ixsystems.com>
2016-09-27 15:12:41 -0400

    Bump Mono's support for large memory further

--- lang/mono/files/patch-mono_io-layer_handles-private.h
+++ lang/mono/files/patch-mono_io-layer_handles-private.h
@@ -5,7 +5,7 @@
  #include <mono/utils/atomic.h>
  
 -#define _WAPI_PRIVATE_MAX_SLOTS		(1024 * 16)
-+#define _WAPI_PRIVATE_MAX_SLOTS		(1024 * 17)
++#define _WAPI_PRIVATE_MAX_SLOTS		(1024 * 32)
  #define _WAPI_PRIVATE_HANDLES(x) (_wapi_private_handles [x / _WAPI_HANDLE_INITIAL_COUNT][x % _WAPI_HANDLE_INITIAL_COUNT])
  #define _WAPI_PRIVATE_HAVE_SLOT(x) ((GPOINTER_TO_UINT (x) / _WAPI_PRIVATE_MAX_SLOTS) < _WAPI_PRIVATE_MAX_SLOTS && \
  					_wapi_private_handles [GPOINTER_TO_UINT (x) / _WAPI_HANDLE_INITIAL_COUNT] != NULL)
