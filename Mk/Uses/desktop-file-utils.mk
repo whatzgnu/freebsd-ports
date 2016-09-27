4633815b0f170ad987f3d4d0b7f9fb283320a610
Kris Moore <kris@ixsystems.com>
2016-09-27 11:19:09 -0400

    Revert the change which disabled desktop-database, needed for a few ports to build

--- Mk/Uses/desktop-file-utils.mk
+++ Mk/Uses/desktop-file-utils.mk
@@ -15,15 +15,8 @@ _INCLUDE_USES_DESKTOP_FILE_UTILS_MK=	yes
 IGNORE=	USES=desktop-file-utils does not require args
 .endif
 
-# Disable this on the TrueOS repo (7/26/16 - Ken Moore)
-#  This basically elevates GTK2 as a requirement for all graphical pkgs
-#  and this behaviour (updating *.desktop databases) is *not* needed for 
-#  many non-GNOME DE's.
-#
-#  Re-evaluate this functionality if we can find an alternative C/C++  tool
-
-#BUILD_DEPENDS+=	update-desktop-database:devel/desktop-file-utils
-#RUN_DEPENDS+=	update-desktop-database:devel/desktop-file-utils
-#PLIST_FILES+=	"@desktop-file-utils"
+BUILD_DEPENDS+=	update-desktop-database:devel/desktop-file-utils
+RUN_DEPENDS+=	update-desktop-database:devel/desktop-file-utils
+PLIST_FILES+=	"@desktop-file-utils"
 
 .endif
